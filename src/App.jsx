07669833fcab7b5b724226bc27348637672c9fe1
MrCamoga <mrcamoga@gmail.com>
2025-07-09 20:37:45 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 function App() {
   return (
-    <>
-      <h1>Tiendita online</h1>
-      <LoginForm />
-    </>
+    <Router>
+      {/* <Header /> */}
+      <main>
+        <h1>Tiendita online</h1>
+        <LoginForm />
+        <Routes>
+          {/* <Route path='/' element={<HomeView />} />
+          <Route path='/home' element={<HomeView />} />
+          <Route path='/login' element={<LoginView />} />
+          <Route path='/register' element={<RegisterView />} />
+          <Route path='/profile' element={<ProfileView />} />
+          <Route path='/search' element={<SearchView />} />
+          <Route path='/cart' element={<CartView />} /> */}
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </Router>
   );
 }
 
