6cbd344e175708bfba2ce9cb1327179001438a05
MrCamoga <mrcamoga@gmail.com>
2025-07-09 20:37:45 +0200

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>Tiendita online</h1>
-    </>
+    <Router>
+      {/* <Header /> */}
+      <main>
+        <Routes>
+          {/* <Route path='/' element={<HomeView />} />
+          <Route path='/home' element={<HomeView />} />
+          <Route path='/login' element={<LoginView />} />
+          <Route path='/register' element={<RegisterView />} />
+          <Route path='/profile' element={<ProfileView />} />
+          <Route path='/search' element={<SearchView />} />
+          <Route path='/cart' element={<CartView />} /> */}
+        </Routes>
+      </main>
+      {/* <Footer /> */}
+    </Router>
   )
 }
 
