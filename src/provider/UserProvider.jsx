c1dac570111fb043367eeda35f46b77b73087b7f
MrCamoga <mrcamoga@gmail.com>
2025-07-13 17:19:22 +0200

    Api url constant

--- src/provider/UserProvider.jsx
+++ src/provider/UserProvider.jsx
@@ -2,13 +2,15 @@ import axios from "axios";
 import { UserContext } from "../context/UserContext";
 import { useState, useEffect } from "react";
 
+const API_URL = 'http://localhost:3000'
+
 export const UserProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [user, setUser] = useState(null);
 
   const login = async (email, passwd) => {
     try {
-      const res = await axios.post("http://localhost:3000/user/login", { email, passwd });
+      const res = await axios.post(`${API_URL}/user/login`, { email, passwd });
       const token = res.data.token;
       localStorage.setItem("token", token);
       setToken(token);
@@ -20,7 +22,7 @@ export const UserProvider = ({ children }) => {
 
   const fetchProfile = async (authToken = token) => {
     try {
-      const res = await axios.get("http://localhost:3000/user/me", {
+      const res = await axios.get(`${API_URL}/user/me`, {
         headers: { Authorization: authToken },
       });
       setUser(res.data);
