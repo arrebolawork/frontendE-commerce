0df7c92ad1c3737c7ae97921fd678c1c84ffd737
MrCamoga <mrcamoga@gmail.com>
2025-07-14 20:29:56 +0200

    Funcionalidad carro

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,39 +7,44 @@ import './Header.scss'
 import UserMenu from './UserMenu.jsx'
 
 const Header = () => {
-    const { cart } = useContext(UserContext)
-    const [cartEmpty,setCartEmpty] = useState(!cart || cart.size == 0)
-    const [showMenu,setShowMenu] = useState(false)
+    const { cart, addProductToCart, emptyCart } = useContext(UserContext);
+    const [cartEmpty, setCartEmpty] = useState(!cart || cart.size == 0);
+    const [showMenu, setShowMenu] = useState(false);
 
     useEffect(() => {
-        setCartEmpty(!cart || cart.size == 0)
-    }, [cart])
+      setCartEmpty(!cart || cart.size == 0);
+    }, [cart]);
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const submitSearch = (event) => {
-        event.preventDefault()
-        const text = event.target.search.value
-        navigate(`/search?name=${text}`)
-    }
+      event.preventDefault();
+      const text = event.target.search.value;
+      navigate(`/search?name=${text}`);
+    };
 
     return (
-        <header className='header'>
-            <form className='search-form' onSubmit={submitSearch}>
-                <input type='text' placeholder='Buscar' name='search' />
-                <button className='search-button' type='submit'></button>
-            </form>
-            <Link to='/'>Inicio</Link>
-            <Link to='/shop'>Tienda</Link>
-            <Link to='/cart' className='header-button icon-cart'>
-                <span className={cartEmpty ? null:'notification'}></span>
-            </Link>
-            <button className='header-button icon-user' onClick={() => {setShowMenu(true)}}>
-                <span className=''></span>
-                {showMenu ? <UserMenu />:null}
-            </button>
-        </header>
-    )
+      <header className="header">
+        <form className="search-form" onSubmit={submitSearch}>
+          <input type="text" placeholder="Buscar" name="search" />
+          <button className="search-button" type="submit"></button>
+        </form>
+        <Link to="/">Inicio</Link>
+        <Link to="/shop">Tienda</Link>
+        <Link to="/cart" className="header-button icon-cart">
+          <span className={cartEmpty ? null : 'notification'}></span>
+        </Link>
+        <button
+          className="header-button icon-user"
+          onClick={() => {
+            setShowMenu(true);
+          }}
+        >
+          <span className=""></span>
+          {showMenu ? <UserMenu /> : null}
+        </button>
+      </header>
+    );
 }
 
 export default Header
--- src/provider/UserProvider.jsx
+++ src/provider/UserProvider.jsx
@@ -7,16 +7,19 @@ const API_URL = 'http://localhost:3000'
 export const UserProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
   const [user, setUser] = useState(null);
+  const [cart, setCart] = useState(
+    new Map(JSON.parse(localStorage.getItem('cart')) ?? []),
+  );
 
   const login = async (email, passwd) => {
     try {
       const res = await axios.post(`${API_URL}/user/login`, { email, passwd });
       const token = res.data.token;
-      localStorage.setItem("token", token);
+      localStorage.setItem('token', token);
       setToken(token);
       await fetchProfile(token);
     } catch (error) {
-      console.error("Error en login:", error.response?.data || error);
+      console.error('Error en login:', error.response?.data || error);
     }
   };
 
@@ -27,20 +30,39 @@ export const UserProvider = ({ children }) => {
       });
       setUser(res.data);
     } catch (error) {
-      console.error("Error al obtener perfil:", error.response?.data || error);
+      console.error('Error al obtener perfil:', error.response?.data || error);
       logout();
     }
   };
 
   const logout = () => {
-    localStorage.removeItem("token");
+    localStorage.removeItem('token');
     setUser(null);
-    setToken("");
+    setToken('');
+  };
+
+  const addProductToCart = (product, quantity = 1) => {
+    const newCart = new Map(cart);
+    let currentQuantity = newCart.get(product.id) ?? 0;
+    newCart.set(product.id, currentQuantity + quantity);
+    setCart(newCart);
+    localStorage.setItem('cart', JSON.stringify(newCart.entries().toArray()));
+  };
+
+  const emptyCart = () => {
+    setCart(new Map());
+    localStorage.removeItem('cart');
   };
 
   useEffect(() => {
     if (token) fetchProfile();
   }, []);
 
-  return <UserContext.Provider value={{ user, token, login, logout }}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider
+      value={{ user, token, login, logout, cart, addProductToCart, emptyCart }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
