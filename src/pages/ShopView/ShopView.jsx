5ef596c8a38ed49dd577883ef0a90f713beac01b
MrCamoga <mrcamoga@gmail.com>
2025-07-19 10:53:05 +0200

    Refactor product component

--- src/pages/ShopView/ShopView.jsx
+++ src/pages/ShopView/ShopView.jsx
@@ -2,9 +2,8 @@ import ProductList from '../../components/ProductWidget/ProductWidget';
 import { ProductContext } from '../../context/ProductContext';
 import './ShopView.scss';
 import { useContext, useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
+
 const ShopView = () => {
-  // const location = useLocation();
   const { products, getAllProducts } = useContext(ProductContext);
   useEffect(() => {
     getAllProducts();
